5fe84cfd6fc6542c6ee0e66816b2effca871b031
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-05-12 15:13:55 -0400

    testing with react and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+        <Form className='mb-3'>
+          <Row>
+            <Col md>
+            <Form.Group controlId='formEmail'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='Example@email.com' />
+            <Form.Text className='text-muted'>
+              We'll never share your email address, trust us!
+            </Form.Text>
+          </Form.Group>
+          </Col>
+          <Col md>
+            <Form.Group controlId='formPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Password' />
+            </Form.Group>
+          </Col>
+          </Row>
+          <Button variant='secondary' type='submit'>Login</Button>
+        </Form>
+
+        <Card className='mb-3' style={{ color: '#000'}}>
+          <Card.Img src='https://picsum.photos/200/80' />
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              This is an example of react boostrap cards
+            </Card.Text>
+            <Button variant='primary'>Read More</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <BreadcrumbItem>Test</BreadcrumbItem>
+          <BreadcrumbItem>Test 2</BreadcrumbItem>
+          <BreadcrumbItem active>Test 3</BreadcrumbItem>
+        </Breadcrumb>
+        <Alert variant='success' >This a Button</Alert>
+        <Button>Test Button</Button>
+        </Container>
       </header>
     </div>
   );
